34431af43966822da2f5e073eaf15b928dd7d540
elemarbar2 <elemarbar2@alu.edu.gva.es>
2025-11-05 10:02:50 +0100

    push

---
+++ src/main/java/Practica4.3.java
@@ -0,0 +1,18 @@
+public class Practica {
+    public static void main(String[] args) {
+        import java.util.Scanner;
+        void main() {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Introduce tu año de nacimiento:");
+            int anyo = sc.nextInt();
+            LocalDateTime fechaActual = LocalDateTime.now();
+            int edad = fechaActual.getYear()+anyo;
+            if(edad>18){
+                System.out.println("Eres mayor de edad");
+            }
+            else{
+                System.out.println("Eres menor de edad");
+            }
+        }
+    }
+}
